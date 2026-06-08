08c84bef9f03656ad351f336d80d42740123abd3
Alireza Pishdar <pishdaralirza@gmail.com>
2026-06-09 01:39:15 +0430

    parsing command line

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from feeds!")
+import argparse
+from pathlib import Path
 
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="feeds",
+                description="downloading and storing RSS feeds")
+    parser.add_argument("-n", "--name", action="store_true",
+                        help="show feed group names")
+    parser.add_argument("-s", "--sync", nargs="+", metavar="name",
+                        help="download RSS feeds, parse them, and store titles/links")
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == "__main__":
     main()
